aa8e3119f05cc8c7df3427c28b0cfd09ecd73c1a
Rohan Pandit <rohananandpandit@gmail.com>
2025-05-20 06:36:19 +0100

    feat: configure host and port for FastMCP server

--- src/mcp_server.py
+++ src/mcp_server.py
@@ -4,6 +4,10 @@ from utils.client import Trading212Client
 
 load_dotenv(find_dotenv())
 
-mcp = FastMCP(name="trading212", dependencies=["hishel", "pydantic"])
+mcp = FastMCP(name="Trading212", 
+              dependencies=["hishel", "pydantic"], 
+              stateless_http=True, 
+              host="127.0.0.1", 
+              port=8000)
 
 client = Trading212Client()
